9dc2b84a39d26e24bbd3dce1eb0477616fd9a9d8
Zhanghao <243382542@qq.com>
2018-01-11 22:37:01 +0800

    init

---
+++ js/urlHead.js
@@ -0,0 +1,6 @@
+ const URL = {
+  // testHead: "http://192.168.18.43:8089",
+  testHead: "http://app2.henanga.gov.cn",
+  getDeptUser: "/jmth5/zzga/wxQRCode/getDeptUser",
+  getSubscribedDetials: "/jmth5/zzga/wxQRCode/getSubscribedDetials",
+ }
